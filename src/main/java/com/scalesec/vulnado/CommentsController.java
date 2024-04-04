18be29b74c1e237e5fe10b3a232d91710e824006
julioarrudagft <152617514+julioarrudagft@users.noreply.github.com>
2024-04-04 11:02:02 -0300

    [GFTCodeFix]: Update on  src/main/java/com/scalesec/vulnado/CommentsController.java

--- src/main/java/com/scalesec/vulnado/CommentsController.java
+++ src/main/java/com/scalesec/vulnado/CommentsController.java
@@ -13,29 +13,47 @@ public class CommentsController {
   @Value("${app.secret}")
   private String secret;
 
-  @CrossOrigin(origins = "*")
-  @RequestMapping(value = "/comments", method = RequestMethod.GET, produces = "application/json")
+  // Make sure that enabling CORS is safe here. If it's not, remove the @CrossOrigin annotation or restrict origins.
+  @CrossOrigin(origins = "http://example.com") // Replace "*" with the actual origin or remove if CORS is not needed
+  @GetMapping(value = "/comments", produces = "application/json")
   List<Comment> comments(@RequestHeader(value="x-auth-token") String token) {
     User.assertAuth(secret, token);
     return Comment.fetch_all();
   }
 
-  @CrossOrigin(origins = "*")
-  @RequestMapping(value = "/comments", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
+  @CrossOrigin(origins = "http://example.com") // Replace "*" with the actual origin or remove if CORS is not needed
+  @PostMapping(value = "/comments", produces = "application/json", consumes = "application/json")
   Comment createComment(@RequestHeader(value="x-auth-token") String token, @RequestBody CommentRequest input) {
-    return Comment.create(input.username, input.body);
+    return Comment.create(input.getUsername(), input.getBody());
   }
 
-  @CrossOrigin(origins = "*")
-  @RequestMapping(value = "/comments/{id}", method = RequestMethod.DELETE, produces = "application/json")
+  @CrossOrigin(origins = "http://example.com") // Replace "*" with the actual origin or remove if CORS is not needed
+  @DeleteMapping(value = "/comments/{id}", produces = "application/json")
   Boolean deleteComment(@RequestHeader(value="x-auth-token") String token, @PathVariable("id") String id) {
     return Comment.delete(id);
   }
 }
 
 class CommentRequest implements Serializable {
-  public String username;
-  public String body;
+  private static final long serialVersionUID = 1L;
+  private String username;
+  private String body;
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getBody() {
+    return body;
+  }
+
+  public void setBody(String body) {
+    this.body = body;
+  }
 }
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
