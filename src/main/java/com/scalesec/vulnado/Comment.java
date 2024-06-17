0197d81234f6422722aec8da5e99ef181a81f31a
julioarrudagft <152617514+julioarrudagft@users.noreply.github.com>
2024-06-17 20:39:13 -0300

    Update Comment.java

--- src/main/java/com/scalesec/vulnado/Comment.java
+++ src/main/java/com/scalesec/vulnado/Comment.java
@@ -14,7 +14,7 @@ public class Comment {
   public Comment(String id, String username, String body, Timestamp created_on) {
     this.id = id;
     this.username = username;
-    this.body = body;//
+    this.body = body;
     this.created_on = created_on;
   }
 
