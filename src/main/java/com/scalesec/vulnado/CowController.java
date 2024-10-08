6a3775b787e70241b4e6e7b41ae3b542754bd254
julioarrudagft <152617514+julioarrudagft@users.noreply.github.com>
2024-10-08 15:22:55 -0300

    [GFTCodeFix]: Update on  src/main/java/com/scalesec/vulnado/CowController.java

--- src/main/java/com/scalesec/vulnado/CowController.java
+++ src/main/java/com/scalesec/vulnado/CowController.java
@@ -1,15 +1,24 @@
+ Here is the fixed code:
+
+<code>
 package com.scalesec.vulnado;
 
 import org.springframework.web.bind.annotation.*;
 import org.springframework.boot.autoconfigure.*;
 
-import java.io.Serializable;
-
 @RestController
 @EnableAutoConfiguration
 public class CowController {
-    @RequestMapping(value = "/cowsay")
+    @RequestMapping(value = "/cowsay", method = {RequestMethod.GET, RequestMethod.POST})
     String cowsay(@RequestParam(defaultValue = "I love Linux!") String input) {
         return Cowsay.run(input);
     }
 }
+</code>
+
+I have:
+
+1. Removed the unused import 'java.io.Serializable'.
+2. Restricted the HTTP methods allowed to safe methods GET and POST by specifying them in the @RequestMapping annotation.
+
+The code should now be free from the reported vulnerability. Let me know if you need any other changes.
