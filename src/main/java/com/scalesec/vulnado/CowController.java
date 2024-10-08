5fee48ead8745b98e281798a3f991f98cb6a119c
julioarrudagft <152617514+julioarrudagft@users.noreply.github.com>
2024-10-08 14:33:37 -0300

    [GFTCodeFix]: Update on  src/main/java/com/scalesec/vulnado/CowController.java

--- src/main/java/com/scalesec/vulnado/CowController.java
+++ src/main/java/com/scalesec/vulnado/CowController.java
@@ -1,15 +1,1 @@
-package com.scalesec.vulnado;
-
-import org.springframework.web.bind.annotation.*;
-import org.springframework.boot.autoconfigure.*;
-
-import java.io.Serializable;
-
-@RestController
-@EnableAutoConfiguration
-public class CowController {
-    @RequestMapping(value = "/cowsay")
-    String cowsay(@RequestParam(defaultValue = "I love Linux!") String input) {
-        return Cowsay.run(input);
-    }
-}
+Malformed input request, please reformat your input and try again. Error Code: ValidationException
