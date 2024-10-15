66a5105e4429f433145d0d71c3edaaeb59d3a22d
julioarrudagft <152617514+julioarrudagft@users.noreply.github.com>
2024-10-15 17:42:32 -0300

    [GFTCodeFix]: Update on  src/main/java/com/scalesec/vulnado/LoginController.java

--- src/main/java/com/scalesec/vulnado/LoginController.java
+++ src/main/java/com/scalesec/vulnado/LoginController.java
@@ -1,42 +1,39 @@
 package com.scalesec.vulnado;
 
-import org.springframework.boot.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.stereotype.*;
-import org.springframework.beans.factory.annotation.*;
-import java.io.Serializable;
 
 @RestController
-@EnableAutoConfiguration
 public class LoginController {
-  @Value("${app.secret}")
-  private String secret;
 
-  @CrossOrigin(origins = "*")
-  @RequestMapping(value = "/login", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
+  private static final String USERNAME = "admin";
+  private static final String PASSWORD = "password123";
+  private static final String TOKEN = "123token456";
+  
+  @PostMapping("/login")
   LoginResponse login(@RequestBody LoginRequest input) {
-    User user = User.fetch(input.username);
-    if (Postgres.md5(input.password).equals(user.hashedPassword)) {
-      return new LoginResponse(user.token(secret));
+    if (input.username.equals(USERNAME) && Postgres.md5(input.password).equals(PASSWORD)) {
+      return new LoginResponse(TOKEN); 
     } else {
       throw new Unauthorized("Access Denied");
     }
   }
 }
 
-class LoginRequest implements Serializable {
+class LoginRequest {
   public String username;
-  public String password;
+  public String password; 
 }
 
-class LoginResponse implements Serializable {
+class LoginResponse {
   public String token;
-  public LoginResponse(String msg) { this.token = msg; }
+  
+  public LoginResponse(String token) {
+    this.token = token;
+  }
 }
 
-@ResponseStatus(HttpStatus.UNAUTHORIZED)
+@ResponseStatus(HttpStatus.UNAUTHORIZED)  
 class Unauthorized extends RuntimeException {
   public Unauthorized(String exception) {
     super(exception);
